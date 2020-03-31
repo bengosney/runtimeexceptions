cf7ca6608590fc9ea54ca077f86152ce7a9a29bf
Ben Gosney <bengosney@googlemail.com>
2020-03-31 08:43:56 +0100

    Cleaner pip list

--- runtimeexceptions/settings.py
+++ runtimeexceptions/settings.py
@@ -38,14 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'debug_toolbar',
     'strava',
     'websettings',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
