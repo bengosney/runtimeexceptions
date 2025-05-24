d1f6b09b51f5aa55981f7471b3e932ffa8c21c8d
Ben Gosney <bengosney@googlemail.com>
2025-05-24 20:55:31 +0100

    fix: remove unused authentication backend from settings

--- runtimeexceptions/settings/base.py
+++ runtimeexceptions/settings/base.py
@@ -62,7 +62,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 LANGUAGE_CODE = "en-gb"
