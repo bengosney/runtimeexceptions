bea2fe4cc42eeb0682ef95e884a37c65a8c44571
Ben Gosney <bengosney@googlemail.com>
2025-05-26 21:22:35 +0100

    fix: update commit hash in .copier-answers.yml and change STATIC_ROOT to STATICFILES_DIRS in base.py

--- runtimeexceptions/settings/base.py
+++ runtimeexceptions/settings/base.py
@@ -74,7 +74,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
