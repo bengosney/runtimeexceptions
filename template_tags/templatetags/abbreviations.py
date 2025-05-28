115b0b08daf246c58d9239ce608718c5a9fb5771
Ben Gosney <bengosney@googlemail.com>
2025-05-28 21:09:03 +0100

    feat: add template tag to shorten time representations

---
+++ template_tags/templatetags/abbreviations.py
@@ -0,0 +1,25 @@
+import re
+
+from django import template
+
+register = template.Library()
+
+_ABBREVIATIONS = {
+    "minute": "m",
+    "second": "s",
+    "hour": "h",
+    "day": "d",
+    "and": " ",
+}
+
+
+@register.filter
+def shorten_time(value):
+    """Shorten time to a more readable format."""
+
+    def repl(match):
+        return _ABBREVIATIONS[match.group(1).lower()]
+
+    regex = r"\b\s(" + "|".join(map(re.escape, _ABBREVIATIONS.keys())) + r")s?\b"
+    pattern = re.compile(regex)
+    return pattern.sub(repl, value)
