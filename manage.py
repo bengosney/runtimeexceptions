880e9ded10d88a4d9ea3b4f55c5d0f3411670587
Ben Gosney <bengosney@googlemail.com>
2025-06-04 19:51:26 +0100

    feat: add ngrok integration for local development and cleanup on exit

--- manage.py
+++ manage.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 
+import atexit
 import os
 import sys
 
 
+def exit_handler():
+    print("Cleaning up ngrok file...")
+    file_path = ".ngrok"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
 def main():
+    if os.getenv("NGROK_LISTENER_RUNNING") is None and os.getenv("NGROK_AUTHTOKEN") is not None:
+        os.environ["NGROK_LISTENER_RUNNING"] = "true"
+        import asyncio, ngrok  # noqa: E401, I001
+
+        async def setup():
+            listen = sys.argv[2] if len(sys.argv) > 2 else "localhost:8000"  # noqa: PLR2004
+            try:
+                listener = await ngrok.default()
+                print(f"Forwarding to {listen} from ingress url: {listener.url()}")
+                listener.forward(listen)
+                with open(".ngrok", "w") as f:
+                    f.write(listener.url())
+                atexit.register(exit_handler)
+            except ValueError:
+                pass
+
+        asyncio.run(setup())
+
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "runtimeexceptions.settings.dev")
     try:
