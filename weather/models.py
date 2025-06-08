66311d20482f23afc05c03bf39ed77bf00ec9ef1
Ben Gosney <bengosney@googlemail.com>
2025-06-08 21:06:23 +0100

    feat: improve the long weather

--- weather/models.py
+++ weather/models.py
@@ -31,13 +31,50 @@ class Weather(models.Model):
         """
         Returns a short string representation of the weather.
         """
-        return f"{self.status} at {self.temperature}°C"
+        return f"{self.status} {self.temperature}°C"
 
     def long(self) -> str:
         """
         Returns a long string representation of the weather.
         """
-        return f"{self.detailed_status}, {self.temperature}°C (feels like {self.temperature_feels_like}°C)"
+        return f"{self.detailed_status}, {self.temperature_celsius()}, {self.humidity_percentage()}, {self.wind()}"
+
+    def temperature_celsius(self) -> str:
+        """
+        Returns the temperature in Celsius.
+        """
+        return f"{self.temperature:.1f}°C feels like {self.temperature_feels_like:.1f}°C"
+
+    def humidity_percentage(self) -> str:
+        """
+        Returns the humidity as a percentage.
+        """
+        return f"Humidity {self.humidity:.0f}%"
+
+    @staticmethod
+    def degrees_to_cardinal(d):
+        """
+        Converts degrees to cardinal direction.
+        """
+        dirs = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+        ix = round(d / (360.0 / len(dirs)))
+        return dirs[ix % len(dirs)]
+
+    @staticmethod
+    def mps_to_kph(meters_per_second):
+        """
+        Converts meters per second to miles per hour.
+        """
+        return meters_per_second * 3.6
+
+    def wind(self) -> str:
+        """
+        Returns the wind speed and direction.
+        """
+        speed = self.mps_to_kph(self.wind_speed)
+        direction = self.degrees_to_cardinal(self.wind_direction)
+        gusts = self.mps_to_kph(self.wind_gust)
+        return f"Wind {speed:.1f}km/h from {direction}, gusting up to {gusts:.1f}km/h"
 
     @classmethod
     def from_lat_long(cls, latitude: float, longitude: float) -> Self:
