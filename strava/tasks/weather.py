b8da6581d06bcb57976c669ac909518e7459a077
Ben Gosney <bengosney@googlemail.com>
2025-07-20 21:24:26 +0100

    refactor(datamodels): use a more standard way of generating data types

--- strava/tasks/weather.py
+++ strava/tasks/weather.py
@@ -1,13 +1,12 @@
 from django_tasks import task
 
+from strava.data_models import ActivityType
 from strava.models import Activity, Event
-from strava.types import ActivityType
 
 valid_activity_types = [
-    ActivityType.RUN.value,
-    ActivityType.RIDE.value,
-    ActivityType.TRAILRUN.value,
-    ActivityType.WALK.value,
+    ActivityType.Run.value,
+    ActivityType.Ride.value,
+    ActivityType.Walk.value,
 ]
 
 
