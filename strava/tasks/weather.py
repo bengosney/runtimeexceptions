20690e1880403083db3270d6c32fe173fb7475a7
Ben Gosney <bengosney@googlemail.com>
2025-07-23 20:05:12 +0100

    feat(log): add a load more logging

--- strava/tasks/weather.py
+++ strava/tasks/weather.py
@@ -1,7 +1,11 @@
+import logging
+
 from django_tasks import task
 
 from strava.models import Activity, Event
 from strava.types import ActivityType
+
+logger = logging.getLogger(__name__)
 
 valid_activity_types = [
     ActivityType.RUN.value,
@@ -14,11 +18,27 @@ valid_activity_types = [
 @task
 def set_weather(update_id: int):
     event = Event.objects.get(id=update_id)
+    logger.info(
+        "Setting weather for event",
+        extra={
+            "event": event,
+            "aspect_type": getattr(event, "aspect_type", None),
+        },
+    )
     if getattr(event, "aspect_type", None) != Event.ASPECT_TYPES["create"]:
+        logger.info("Event is not a create event, skipping weather update")
         return
 
     activity = Activity.find_or_create(event.owner_id, event.object_id)
+    logger.info(
+        "Found or created activity",
+        extra={
+            "activity": activity,
+            "weather": activity.weather,
+            "type": activity.type,
+        },
+    )
 
     if activity.weather and activity.type in valid_activity_types:
-        print("setting weather for activity", activity.strava_id)
+        logger.info("Setting weather for activity", extra={"activity": activity})
         activity.add_weather()
