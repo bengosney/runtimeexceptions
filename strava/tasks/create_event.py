217b017b4d8da7a6d9c85c87ec16c4ccfc8df111
Ben Gosney <bengosney@googlemail.com>
2025-07-31 19:54:22 +0100

    feat: refactor create_event task to use a pydantic model for data validation

--- strava/tasks/create_event.py
+++ strava/tasks/create_event.py
@@ -1,10 +1,11 @@
 import logging
 from datetime import datetime
-from typing import Any
+from typing import Any, Literal
 
 from django.utils.timezone import make_aware
 
 from django_tasks import task
+from pydantic import BaseModel
 
 from strava.models import Event, Runner
 from strava.tasks.update_activity_weather import update_activity_weather
@@ -12,10 +13,39 @@ from strava.tasks.update_activity_weather import update_activity_weather
 logger = logging.getLogger(__name__)
 
 
+class WebHook(BaseModel):
+    object_type: Literal["activity", "athlete"]
+    object_id: int
+    aspect_type: Literal["create", "update", "delete"]
+    updates: dict[str, Any] | None = None
+    owner_id: str
+    subscription_id: int
+    event_time: int
+
+    class Config:
+        frozen = True
+
+
+def transform_webhook_data_to_event(webhook: WebHook) -> Event:
+    event_time = make_aware(datetime.fromtimestamp(webhook.event_time))
+    owner_id = Runner.objects.get(strava_id=webhook.owner_id)
+    return Event(
+        object_type=webhook.object_type,
+        object_id=webhook.object_id,
+        aspect_type=webhook.aspect_type,
+        updates=webhook.updates,
+        owner_id=owner_id,
+        subscription_id=webhook.subscription_id,
+        event_time=event_time,
+    )
+
+
 @task
 def create_event(**kwargs: Any):
-    kwargs["event_time"] = make_aware(datetime.fromtimestamp(kwargs["event_time"]))
-    kwargs["owner_id"] = Runner.objects.get(strava_id=kwargs["owner_id"])
+    webhook = WebHook.model_validate(kwargs)
+
+    event = transform_webhook_data_to_event(webhook)
+
     logger.info("Creating event with kwargs: %s", kwargs)
     event = Event.objects.create(**kwargs)
     logger.info("Event created: %d", event.pk)
---
+++ strava/tests/test_webhook.py
@@ -0,0 +1,54 @@
+import pytest
+from model_bakery import baker
+
+from strava.models import Event, Runner
+from strava.tasks.create_event import WebHook, transform_webhook_data_to_event
+
+OBJECT_TYPE = "activity"
+OBJECT_ID = 123
+ASPECT_TYPE = "create"
+UPDATES = {"foo": "bar"}
+OWNER_ID = "456"
+SUBSCRIPTION_ID = 789
+EVENT_TIME = 1620000000
+
+
+@pytest.fixture
+def webhook_data():
+    return {
+        "object_type": OBJECT_TYPE,
+        "object_id": OBJECT_ID,
+        "aspect_type": ASPECT_TYPE,
+        "updates": UPDATES,
+        "owner_id": OWNER_ID,
+        "subscription_id": SUBSCRIPTION_ID,
+        "event_time": EVENT_TIME,
+    }
+
+
+def test_webhook_valid(webhook_data):
+    webhook = WebHook.model_validate(webhook_data)
+    assert webhook.object_type == OBJECT_TYPE
+    assert webhook.object_id == OBJECT_ID
+    assert webhook.aspect_type == ASPECT_TYPE
+    assert webhook.updates == UPDATES
+    assert webhook.owner_id == OWNER_ID
+    assert webhook.subscription_id == SUBSCRIPTION_ID
+    assert webhook.event_time == EVENT_TIME
+
+
+@pytest.mark.django_db
+def test_transform_webhook_data_to_event(webhook_data):
+    runner = baker.make(Runner, strava_id=OWNER_ID)
+
+    # Transform the webhook data to an Event
+    event = transform_webhook_data_to_event(WebHook.model_validate(webhook_data))
+
+    assert isinstance(event, Event)
+    assert event.object_type == OBJECT_TYPE
+    assert event.object_id == OBJECT_ID
+    assert event.aspect_type == ASPECT_TYPE
+    assert event.updates == UPDATES
+    assert event.owner_id == runner
+    assert event.subscription_id == SUBSCRIPTION_ID
+    assert event.event_time.timestamp() == EVENT_TIME
