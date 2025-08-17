09cc55eaebcb28199b408fec71819ce32774281c
Ben Gosney <bengosney@googlemail.com>
2025-08-17 21:41:33 +0100

    test: fix create event test

--- strava/tests/test_create_event.py
+++ strava/tests/test_create_event.py
@@ -10,7 +10,8 @@ from strava.tasks.create_event import create_event as create_event_func
 
 @pytest.mark.django_db
 @patch("strava.tasks.create_event.update_activity_weather")
-def test_create_event_success(mock_update_weather, db):
+@patch("strava.tasks.create_event.update_triathlon_score")
+def test_create_event_success(mock_update_triathlon, mock_update_weather, db):
     runner = baker.make(Runner, strava_id="strava123")
     kwargs = {
         "event_time": int(datetime.now().timestamp()),
@@ -26,3 +27,4 @@ def test_create_event_success(mock_update_weather, db):
     event = Event.objects.get(owner=runner, object_id=kwargs["object_id"])
     assert event is not None
     mock_update_weather.enqueue.assert_called_once_with(event.pk)
+    mock_update_triathlon.enqueue.assert_called_once_with(event.owner.pk, event.object_id)
